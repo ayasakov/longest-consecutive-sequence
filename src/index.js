50a3931860d6254c00e0f925b42265a2e7729513
Artem Iasakov <Artem_Iasakov@epam.com>
2017-12-04 18:30:12 +0400

    feat: longest consecutive sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+    const data = {};
+    let result = 1;
+
+    for (let i = 0; i < array.length; i++) {
+        const n = array[i];
+        data[n] = data[n] || 0;
+
+        if (data[n] === 0) {
+            data[n] = 1;
+            const a = data[n - 1] || 0;
+            const b = data[n + 1] || 0;
+            const sum = a + b + data[n];
+
+            result = Math.max(sum, result);
+            data[n - a] = sum;
+            data[n + b] = sum;
+        }
+    }
+
+    return(result === 1 ? 0 : result);
+};
